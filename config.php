6c61acf5d4deb46b367321498324b068ce7903b6
Gbolahan <abass_gbolahan@yahoo.com>
2023-08-03 11:06:20 +0100

    db connection & stripe API

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php 
+ 
+// Product Details 
+// Minimum amount is $0.50 US 
+$itemName = "Demo Product"; 
+$itemPrice = 25;  
+$currency = "USD";  
+ 
+/* Stripe API configuration 
+ * Remember to switch to your live publishable and secret key in production! 
+ * See your keys here: https://dashboard.stripe.com/account/apikeys 
+ */ 
+define('STRIPE_API_KEY', 'sk_test_51NawPmHwpeiZzeqVC38Dnjthy0KNRl0XJX8UnktONH5ygxAjxU1OSZHbD4U6UbbTGOAYIm1qUrrU9PuWO230BDH700ClbGFMdu'); 
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51NawPmHwpeiZzeqVHt0kff2e7lODFULmvoxVmtCQaCbeDjfx66gvRl81364Ghe54KdZCAniE6fMeTJWDK37s0FVh00SFRjOH3c'); 
+  
+// Database configuration  
+define('DB_HOST', 'localhost');  
+define('DB_USERNAME', 'root');  
+define('DB_PASSWORD', 'root');  
+define('DB_NAME', 'codexworld'); 
+ 
+?>
--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
-    <title>Stripe Pay</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+<?php 
+// Include configuration file  
+require_once 'config.php'; 
+?>
